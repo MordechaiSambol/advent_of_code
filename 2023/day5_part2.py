53f3362ee461fbb0b88ec998b81f7e71184bea47
Mordechai_Sambol <mordechai.sambol@gmail.com>
2023-12-05 18:11:27 +0200

    Cleaned up day5 part2 and added explanation to it

--- 2023/day5_part2.py
+++ 2023/day5_part2.py
@@ -1,4 +1,18 @@
-# In the inputfile, make sure that there are *no* \n at the end.
+# In the inputfile, make sure that there are *no* \n at the end. (Note that this is different from day5 part1)
+
+# Explaining code: Each pair of seeds 79 14 represents a range(79, 79+14).
+# In the code I called this a range, but actually had it saved as a tuple (start, end).
+# Each a-to-b map in the input is a "transformation" - each original seed number is sent to a new number.
+# The transformation is built of a set of rules which I saved in a dictionary: {source_range: destination_range}.
+# I applied the first transformation on the original seeds by finding which parts of the seed ranges intersected the
+# source_ranges of the transformation, and which didn't. Then I applied the second transformation on the results
+# of the first one, and so on until the last transformation was done.
+
+# So: 'with open' sets up the input - what is seeds and what are transformations.
+# 'for transformation' iterates through the transformations - for each one it organizes the rules,
+# and then applies them using 'apply_rules'. 'transform_range' does the transformation on a single range.
+
+
 def apply_rules(trans_rules, ranges_to_trans):
     new_ranges = []
     for rule_in, rule_out in trans_rules.items():
@@ -16,26 +30,23 @@ def transform_range(range1, rule_in, rule_out):
     new_ranges = []
     remaining_ranges = []
 
-    if rule_in[0] <= range1[0] <= rule_in[1] <= range1[1]:
-        diff = range1[0] - rule_in[0]
-        new_ranges.append((rule_out[0] + diff, rule_out[1]))
-        if rule_in[1] < range1[1]:
-            remaining_ranges.append((rule_in[1] + 1, range1[1]))
-
-    elif range1[0] <= rule_in[0] <= range1[1] <= rule_in[1]:
-        diff = range1[1] - rule_in[0]
-        new_ranges.append((rule_out[0], rule_out[0] + diff))
-        if range1[0] < rule_in[0]:
-            remaining_ranges.append((range1[0], rule_in[0] - 1))
-
-    elif rule_in[0] <= range1[0] <= range1[1] <= rule_in[1]:
-        diff = range1[0] - rule_in[0]
-        new_ranges.append((rule_out[0] + diff, rule_out[0] + diff + range1[1] - range1[0]))
-
-    else:
+    if range1[0] > rule_in[1] or range1[1] < rule_in[0]:
         remaining_ranges = [range1]
+    else:
+        left_diff = range1[0] - rule_in[0]
+        right_diff = range1[1] - rule_in[1]
 
-    return new_ranges, remaining_ranges
+        # The overlap of range1 with rule_in
+        new_ranges.append(
+            (rule_out[0] + (left_diff if left_diff > 0 else 0), rule_out[1] + (right_diff if right_diff < 0 else 0))
+        )
+        # The parts of range1 that don't overlap with rule_in
+        remaining_ranges.extend([
+            (rule_in[1] + 1, range1[1]),
+            (range1[0], rule_in[0] - 1)
+        ])
+
+    return new_ranges, [x for x in remaining_ranges if x[1] >= x[0]]
 
 
 with open('day5_inputfile.txt') as file:
