74f61cebed03f1953ca935435c247fd8987da428
Mordechai_Sambol <mordechai.sambol@gmail.com>
2023-12-06 09:47:27 +0200

    Removed unnecessary prints

--- 2023/day6.py
+++ 2023/day6.py
@@ -8,16 +8,11 @@ with open('day6_inputfile.txt') as file:
     # times = [int(''.join(re.split(r'\s+', file.readline().strip())[1:]))]
     # distances = [int(''.join(re.split(r'\s+', file.readline().strip())[1:]))]
 
-print(f'times: {times}')
-print(f'distances: {distances}')
-
 result = 1
 for time1, distance1 in zip(times, distances):
     lower_boundry = (time1 - math.sqrt(time1 ** 2 - 4 * distance1)) / 2
     upper_boundry = (time1 + math.sqrt(time1 ** 2 - 4 * distance1)) / 2
-    print(lower_boundry, upper_boundry)
     result *= (math.floor(upper_boundry) - math.ceil(lower_boundry) + 1
                - 1 * (upper_boundry % 1 == 0) - 1 * (lower_boundry % 1 == 0))
-    print(result)
 
 print(result)
