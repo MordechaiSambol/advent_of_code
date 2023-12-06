14280b3bdee5b0d3a716172ce56e49caeacd4f49
Mordechai_Sambol <mordechai.sambol@gmail.com>
2023-12-06 09:46:03 +0200

    Day6

---
+++ 2023/day6.py
@@ -0,0 +1,23 @@
+import re, math
+
+with open('day6_inputfile.txt') as file:
+    # Part 1
+    times = [int(x) for x in re.split(r'\s+', file.readline().strip())[1:]]
+    distances = [int(x) for x in re.split(r'\s+', file.readline().strip())[1:]]
+    # For Part 2 replace with the following
+    # times = [int(''.join(re.split(r'\s+', file.readline().strip())[1:]))]
+    # distances = [int(''.join(re.split(r'\s+', file.readline().strip())[1:]))]
+
+print(f'times: {times}')
+print(f'distances: {distances}')
+
+result = 1
+for time1, distance1 in zip(times, distances):
+    lower_boundry = (time1 - math.sqrt(time1 ** 2 - 4 * distance1)) / 2
+    upper_boundry = (time1 + math.sqrt(time1 ** 2 - 4 * distance1)) / 2
+    print(lower_boundry, upper_boundry)
+    result *= (math.floor(upper_boundry) - math.ceil(lower_boundry) + 1
+               - 1 * (upper_boundry % 1 == 0) - 1 * (lower_boundry % 1 == 0))
+    print(result)
+
+print(result)
